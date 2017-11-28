f7e15d027f296bb02b184514773e6478f2757927
Ian Tan <ian.tan@techinasia.com>
2017-11-28 17:30:34 +0800

    Refactor Adapter in order to defer SQL execution

--- expecter_adapter.go
+++ expecter_adapter.go
@@ -2,6 +2,7 @@ package gormexpect
 
 import (
 	"database/sql"
+	"database/sql/driver"
 
 	"github.com/jinzhu/gorm"
 	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
@@ -25,16 +26,9 @@ func init() {
 // Adapter provides an abstract interface over concrete mock database
 // implementations (e.g. go-sqlmock or go-testdb)
 type Adapter interface {
-	ExpectQuery(stmts ...Stmt) ExpectedQuery
+	ExpectQuery(stmt Stmt) Queryer
 	ExpectExec(stmt Stmt) ExpectedExec
 	AssertExpectations() error
-}
-
-// SqlmockAdapter implemenets the Adapter interface using go-sqlmock
-// it is the default Adapter
-type SqlmockAdapter struct {
-	db     *sql.DB
-	mocker sqlmock.Sqlmock
 }
 
 // NewSqlmockAdapter returns a mock gorm.DB and an Adapter backed by
@@ -49,16 +43,62 @@ func NewSqlmockAdapter(dialect string, args ...interface{}) (*gorm.DB, Adapter, 
 	return gormDb, &SqlmockAdapter{db: db, mocker: mock}, nil
 }
 
+// SqlmockAdapter implemenets the Adapter interface using go-sqlmock
+// it is the default Adapter
+type SqlmockAdapter struct {
+	db     *sql.DB
+	mocker sqlmock.Sqlmock
+}
+
 // ExpectQuery wraps the underlying mock method for setting a query
 // expectation. It accepts multiple statements in the event of preloading
-func (a *SqlmockAdapter) ExpectQuery(queries ...Stmt) ExpectedQuery {
-	return &SqlmockQuery{mock: a.mocker, queries: queries}
+func (a *SqlmockAdapter) ExpectQuery(query Stmt) Queryer {
+	expectation := a.mocker.ExpectQuery(query.sql)
+	return &SqlmockQueryer{query: expectation}
 }
 
 // ExpectExec wraps the underlying mock method for setting a exec
 // expectation
 func (a *SqlmockAdapter) ExpectExec(exec Stmt) ExpectedExec {
 	return &SqlmockExec{mock: a.mocker, exec: exec}
+}
+
+// Queryer is returned from ExpectQuery
+// it is used to control the mock database's response
+type Queryer interface {
+	Returns(rows interface{}) Queryer
+	Errors(err error) Queryer
+	Args(args ...driver.Value) Queryer
+}
+
+// SqlmockQueryer implements Queryer
+type SqlmockQueryer struct {
+	query *sqlmock.ExpectedQuery
+}
+
+// Returns will set the low level rows to be returned for a given set of
+// queries
+func (r *SqlmockQueryer) Returns(rows interface{}) Queryer {
+	sqlmockRows, ok := rows.(*sqlmock.Rows)
+
+	if !ok {
+		panic("Unsupported type passed to Returns")
+	}
+
+	expectation := r.query.WillReturnRows(sqlmockRows)
+	return &SqlmockQueryer{query: expectation}
+}
+
+// Errors will return an error as the query result
+func (r *SqlmockQueryer) Errors(err error) Queryer {
+	expectation := r.query.WillReturnError(err)
+	return &SqlmockQueryer{query: expectation}
+}
+
+// Args sets the args that queries should be executed with
+func (r *SqlmockQueryer) Args(args ...driver.Value) Queryer {
+	expectation := r.query.WithArgs(args...)
+	return &SqlmockQueryer{query: expectation}
 }
 
 // AssertExpectations asserts that _all_ expectations for a test have been met
---
+++ expecter_query.go
@@ -0,0 +1,210 @@
+package gormexpect
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/jinzhu/gorm"
+	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
+)
+
+// QueryExpectation represents an expected query that will be executed and can
+// return some rows. It presents a fluent API for chaining calls to other
+// expectations
+type QueryExpectation interface {
+	Returns(value interface{}) QueryExpectation
+	// Error(err error) QueryExpectation
+}
+
+// SqlmockQueryExpectation implements QueryExpectation for go-sqlmock
+// It gets a pointer to Expecter
+type SqlmockQueryExpectation struct {
+	parent *Expecter
+	scope  *gorm.Scope
+}
+
+// Returns accepts an out type which should either be a struct or slice. Under
+// the hood, it converts a gorm model struct to sql.Rows that can be passed to
+// the underlying mock db
+func (q *SqlmockQueryExpectation) Returns(out interface{}) QueryExpectation {
+	scope := (&gorm.Scope{}).New(out)
+	q.scope = scope
+	// call deferred queries, since we now know the expected out value
+	q.callMethods()
+
+	outVal := indirect(reflect.ValueOf(out))
+
+	spew.Dump(q.parent.recorder.stmts)
+
+	destQuery := q.parent.recorder.stmts[0]
+	subQueries := q.parent.recorder.stmts[1:]
+
+	// main query always at the head of the slice
+	q.parent.adapter.ExpectQuery(destQuery).Returns(q.getDestRows(out))
+
+	// subqueries are preload
+	for _, subQuery := range subQueries {
+		if subQuery.preload != "" {
+			if field, ok := scope.FieldByName(subQuery.preload); ok {
+				expectation := q.parent.adapter.ExpectQuery(subQuery)
+				rows, hasRows := q.getRelationRows(outVal.FieldByName(subQuery.preload), subQuery.preload, field.Relationship)
+
+				if hasRows {
+					expectation.Returns(rows)
+				}
+			}
+		}
+	}
+
+	return q
+}
+
+func (q *SqlmockQueryExpectation) getRelationRows(rVal reflect.Value, fieldName string, relation *gorm.Relationship) (*sqlmock.Rows, bool) {
+	var (
+		rows    *sqlmock.Rows
+		columns []string
+	)
+
+	// we need to check for zero values
+	if reflect.DeepEqual(rVal.Interface(), reflect.New(rVal.Type()).Elem().Interface()) {
+		// spew.Printf("FOUND EMPTY INTERFACE FOR %s\r\n", fieldName)
+		return nil, false
+	}
+
+	switch relation.Kind {
+	case "has_one":
+		scope := &gorm.Scope{Value: rVal.Interface()}
+
+		for _, field := range scope.GetModelStruct().StructFields {
+			if field.IsNormal {
+				columns = append(columns, field.DBName)
+			}
+		}
+
+		rows = sqlmock.NewRows(columns)
+
+		// we don't have a slice
+		row := getRowForFields(scope.Fields())
+		rows = rows.AddRow(row...)
+
+		return rows, true
+	case "has_many":
+		elem := rVal.Type().Elem()
+		scope := &gorm.Scope{Value: reflect.New(elem).Interface()}
+
+		for _, field := range scope.GetModelStruct().StructFields {
+			if field.IsNormal {
+				columns = append(columns, field.DBName)
+			}
+		}
+
+		rows = sqlmock.NewRows(columns)
+
+		if rVal.Len() > 0 {
+			for i := 0; i < rVal.Len(); i++ {
+				scope := &gorm.Scope{Value: rVal.Index(i).Interface()}
+				row := getRowForFields(scope.Fields())
+				rows = rows.AddRow(row...)
+			}
+
+			return rows, true
+		}
+
+		return nil, false
+	case "many_to_many":
+		elem := rVal.Type().Elem()
+		scope := &gorm.Scope{Value: reflect.New(elem).Interface()}
+		joinTable := relation.JoinTableHandler.(*gorm.JoinTableHandler)
+
+		for _, field := range scope.GetModelStruct().StructFields {
+			if field.IsNormal {
+				columns = append(columns, field.DBName)
+			}
+		}
+
+		for _, key := range joinTable.Source.ForeignKeys {
+			columns = append(columns, key.DBName)
+		}
+
+		for _, key := range joinTable.Destination.ForeignKeys {
+			columns = append(columns, key.DBName)
+		}
+
+		rows = sqlmock.NewRows(columns)
+
+		// in this case we definitely have a slice
+		if rVal.Len() > 0 {
+			for i := 0; i < rVal.Len(); i++ {
+				scope := &gorm.Scope{Value: rVal.Index(i).Interface()}
+				row := getRowForFields(scope.Fields())
+
+				// need to append the values for join table keys
+				sourcePk := q.scope.PrimaryKeyValue()
+				destModelType := joinTable.Destination.ModelType
+				destModelVal := reflect.New(destModelType).Interface()
+				destPkVal := (&gorm.Scope{Value: destModelVal}).PrimaryKeyValue()
+
+				row = append(row, sourcePk, destPkVal)
+
+				rows = rows.AddRow(row...)
+			}
+
+			return rows, true
+		}
+
+		return nil, false
+	default:
+		return nil, false
+	}
+}
+
+func (q *SqlmockQueryExpectation) getDestRows(out interface{}) *sqlmock.Rows {
+	var columns []string
+	for _, field := range (&gorm.Scope{}).New(out).GetModelStruct().StructFields {
+		if field.IsNormal {
+			columns = append(columns, field.DBName)
+		}
+	}
+
+	rows := sqlmock.NewRows(columns)
+	outVal := indirect(reflect.ValueOf(out))
+
+	// SELECT multiple columns
+	if outVal.Kind() == reflect.Slice {
+		outSlice := []interface{}{}
+
+		for i := 0; i < outVal.Len(); i++ {
+			outSlice = append(outSlice, outVal.Index(i).Interface())
+		}
+
+		for _, outElem := range outSlice {
+			scope := &gorm.Scope{Value: outElem}
+			row := getRowForFields(scope.Fields())
+			rows = rows.AddRow(row...)
+		}
+	} else if outVal.Kind() == reflect.Struct { // SELECT with LIMIT 1
+		row := getRowForFields(q.scope.Fields())
+		rows = rows.AddRow(row...)
+	} else {
+		panic(fmt.Errorf("Can only get rows for slice or struct"))
+	}
+
+	return rows
+}
+
+func (q *SqlmockQueryExpectation) callMethods() {
+	q.parent.gorm = q.parent.gorm.Set("gorm_expect:ret", q.scope.Value)
+	q.parent.gorm.Callback().Query().Before("gorm:preload").Register("gorm_expect:populate_scope_val", populateScopeValueCallback)
+	noop := reflect.ValueOf(q.parent.gorm)
+	for methodName, args := range q.parent.callmap {
+		methodVal := noop.MethodByName(methodName)
+		spew.Dump(methodVal)
+		switch method := methodVal.Interface().(type) {
+		case func(interface{}, ...interface{}) *gorm.DB:
+			method(args[0], args[1:]...)
+		default:
+			fmt.Println("Not a supported method signature")
+		}
+	}
+}
