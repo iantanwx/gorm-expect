acc197fadf60f9c009b54a6d2bc0fa6042980614
Ian Tan <ian.tan@techinasia.com>
2017-12-07 17:59:14 +0800

    Move association related tests into own file

---
+++ association_test.go
@@ -0,0 +1,126 @@
+package gormexpect_test
+
+import (
+	"testing"
+
+	expecter "github.com/iantanwx/gorm-expect"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAssociationModeFind(t *testing.T) {
+	db, expect, err := expecter.NewDefaultExpecter()
+	defer db.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var emails []Email
+	user := &User{Id: 1, Name: "jinzhu"}
+
+	expect.Model(&user).Association("Emails").Find(&emails).Returns([]Email{Email{Email: "jinzhu@gmail.com"}})
+	err = db.Model(&user).Association("Emails").Find(&emails).Error
+
+	assert.Nil(t, expect.AssertExpectations())
+}
+
+func TestAssociationModeAppend(t *testing.T) {
+	db, expect, err := expecter.NewDefaultExpecter()
+	defer db.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	user1 := User{Id: 1, Name: "jinzhu"}
+	user2 := User{Id: 1, Name: "jinzhu"}
+	emails := []Email{Email{UserId: 1, Email: "uhznij@liamg.moc"}}
+	// expected := User{Id: 1, Name: "jinzhu", Emails: emails}
+
+	expect.Model(&user1).Association("Emails").Append(emails).WillSucceed(1, 1)
+	err = db.Model(&user2).Association("Emails").Append(emails).Error
+
+	assert.Nil(t, expect.AssertExpectations())
+	assert.Nil(t, err)
+}
+
+func TestAssociationModeDelete(t *testing.T) {
+	db, expect, err := expecter.NewDefaultExpecter()
+	defer db.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	emails := []Email{Email{UserId: 1, Email: "uhznij@liamg.moc"}}
+	user1 := User{Id: 1, Name: "jinzhu"}
+	user2 := User{Id: 1, Name: "jinzhu"}
+
+	expect.Model(&user1).Association("Emails").Delete(emails).WillSucceed(1, 1)
+	err = db.Model(&user2).Association("Emails").Delete(emails).Error
+
+	assert.Nil(t, expect.AssertExpectations())
+	assert.Nil(t, err)
+	assert.Nil(t, user2.Emails)
+}
+
+func TestAssociationModeClear(t *testing.T) {
+	db, expect, err := expecter.NewDefaultExpecter()
+	defer db.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	emails := []Email{Email{UserId: 1, Email: "uhznij@liamg.moc"}}
+	user := User{Id: 1, Name: "jinzhu", Emails: emails}
+
+	expect.Model(&user).Association("Emails").Clear().WillSucceed(1, 1)
+	err = db.Model(&user).Association("Emails").Clear().Error
+
+	assert.Nil(t, expect.AssertExpectations())
+	assert.Nil(t, err)
+	assert.Equal(t, 0, len(user.Emails))
+}
+
+func TestAssociationModeReplace(t *testing.T) {
+	db, expect, err := expecter.NewDefaultExpecter()
+	defer db.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	oldEmails := []Email{
+		Email{UserId: 1, Email: "jinzhu@gmail.com"},
+		Email{UserId: 1, Email: "uhznij@liamg.moc"},
+	}
+	newEmails := []Email{
+		Email{UserId: 1, Email: "jinzhu@gmail.com"},
+	}
+	user := User{Id: 1, Name: "jinzhu", Emails: oldEmails}
+
+	expect.Model(&user).Association("Emails").Replace(newEmails).WillSucceed(1, 1)
+	err = db.Model(&user).Association("Emails").Replace(newEmails).Error
+
+	assert.Nil(t, expect.AssertExpectations())
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(user.Emails))
+}
+
+func TestAssociationModeCount(t *testing.T) {
+	db, expect, err := expecter.NewDefaultExpecter()
+	defer db.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	user := User{Id: 1}
+
+	expect.Model(&user).Association("Emails").Count().Returns(5)
+	count := db.Model(&user).Association("Emails").Count()
+
+	assert.Nil(t, expect.AssertExpectations())
+	assert.Equal(t, 5, count)
+}
